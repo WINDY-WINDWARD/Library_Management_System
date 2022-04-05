ade9d81c74d07e7161ed4222e8e0f0dca94a2d53
WINDY_WINDWARD <karthik.sarode23@gmail.com>
2022-04-05 11:57:56 +0530

    bootstrap added to login

--- login.php
+++ login.php
@@ -39,13 +39,14 @@ include("functions.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="./bootstrap-4.0.0-dist/css/bootstrap.css">
     <style>
         form{
             display: flex;
             flex-direction: column;
             width: 50%;
             margin: auto;
-            margin-top: 20%;
+            margin-top: 10%;
         }
         a{
             padding: 10px;
@@ -69,5 +70,6 @@ include("functions.php");
             <a href="signup.php">Signup</a>
         </form>
     </div>
+    <script src="./bootstrap-4.0.0-dist/js/bootstrap.js"></script>
 </body>
 </html>
